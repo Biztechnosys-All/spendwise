7f1535211d10e3cdaa3d5166932b5d837f2245c5
Himanshu <himanshu.bhat@gyanai.com>
2024-11-27 10:15:21 +0530

    commit changes related to show selected package and it's price at checkout page

--- Pages/BuyPackage/Checkout.cshtml.cs
+++ Pages/BuyPackage/Checkout.cshtml.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Spendwise_WebApp.Models;
 
 namespace Spendwise_WebApp.Pages.BuyPackage
 {
     public class CheckoutModel : PageModel
     {
-        public void OnGet()
+        private readonly Spendwise_WebApp.DLL.AppDbContext _context;
+
+        public CheckoutModel(Spendwise_WebApp.DLL.AppDbContext context)
         {
+            _context = context;
+        }
+
+        public Package SelectedPackage { get; set; }
+
+        public async Task OnGet(string packageName)
+        {
+            if (!string.IsNullOrEmpty(packageName))
+            {
+                SelectedPackage = await _context.packages.Where(x => x.PackageName.ToLower() == packageName.ToLower()).FirstOrDefaultAsync();
+
+
+            }
         }
     }
 }
--- Pages/BuyPackage/Index.cshtml.cs
+++ Pages/BuyPackage/Index.cshtml.cs
@@ -1,17 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Spendwise_WebApp.Models;
 
 namespace Spendwise_WebApp.Pages.BuyPackage
 {
     public class IndexModel : PageModel
     {
-        public string PackageName { get; set; } = string.Empty;
+        private readonly Spendwise_WebApp.DLL.AppDbContext _context;
 
-        public void OnGet(string packageName)
+        public IndexModel(Spendwise_WebApp.DLL.AppDbContext context)
+        {
+            _context = context;
+        }
+        public string PackageName { get; set; } = string.Empty;
+        public Package SelectedPackage { get; set; }
+
+        public async Task OnGet(string packageName)
         {
             if(!string.IsNullOrEmpty(packageName))
             {
                 PackageName = packageName;
+                SelectedPackage = await _context.packages.Where(x => x.PackageName.ToLower() == packageName.ToLower()).FirstOrDefaultAsync();
+
+
             }
         }
     }
