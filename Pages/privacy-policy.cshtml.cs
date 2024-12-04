7fbdebc5d131794882fd5c641344f17dd80137f7
Himanshu <himanshu.bhat@gyanai.com>
2024-12-04 17:59:31 +0530

    new pages terms and privacy policy and common footer page

---
+++ Pages/privacy-policy.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Spendwise_WebApp.Pages
+{
+    public class privacy_policyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
