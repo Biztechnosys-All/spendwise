d7d6451636912bc99178d0ca0c1be616f53b423a
Niraj Padariya <niraj.p@biztechnosys.com>
2025-04-30 11:44:37 +0530

    Commit the Summary page changes and some other changes

---
+++ Pages/FormationPage/Summary.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Spendwise_WebApp.Pages.FormationPage
+{
+    public class SummaryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
