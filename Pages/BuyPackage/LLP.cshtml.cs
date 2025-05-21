3a784700b5f0862d3e3d318d3ef5e750b0cc2e91
Niraj Padariya <niraj.p@biztechnosys.com>
2025-05-21 17:27:05 +0530

    commit the non resident package code and some other changes

--- Pages/BuyPackage/LLP.cshtml.cs
+++ Pages/BuyPackage/LLP.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Spendwise_WebApp.Models;
 
 namespace Spendwise_WebApp.Pages.BuyPackage
 {
     public class LLPModel : PageModel
     {
-        public void OnGet()
+        private readonly Spendwise_WebApp.DLL.AppDbContext _context;
+
+        public LLPModel(Spendwise_WebApp.DLL.AppDbContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Package Package { get; set; } = default!;
+        public async Task<IActionResult> OnGet()
+        {
+            Package = await _context.packages.FirstOrDefaultAsync(m => m.PackageName.ToLower() == "LLP");
+            return Page();
         }
     }
 }
--- Pages/BuyPackage/Non-residents.cshtml.cs
+++ Pages/BuyPackage/Non-residents.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Spendwise_WebApp.Models;
 
 namespace Spendwise_WebApp.Pages.BuyPackage
 {
     public class Non_residentsModel : PageModel
     {
-        public void OnGet()
+        private readonly Spendwise_WebApp.DLL.AppDbContext _context;
+
+        public Non_residentsModel(Spendwise_WebApp.DLL.AppDbContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Package Package { get; set; } = default!;
+        public async Task<IActionResult> OnGet()
+        {
+            Package = await _context.packages.FirstOrDefaultAsync(m => m.PackageName.ToLower() == "Non-Residents");
+            return Page();
         }
     }
 }
